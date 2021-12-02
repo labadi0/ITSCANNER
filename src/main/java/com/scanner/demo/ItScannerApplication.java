dba4b83704ef95e851d0722d09d833d42ba5f5d0
labadi0 <labaditarekk@gmail.com>
2021-12-02 21:16:20 +0100

    add all project

---
+++ src/main/java/com/scanner/demo/ItScannerApplication.java
@@ -0,0 +1,13 @@
+package com.scanner.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItScannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scanner/demo/ItScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scanner.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
