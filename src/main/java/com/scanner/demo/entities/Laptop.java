a0697a8b5789a10160781e59b9993bfa27a5888f
labadi0 <labaditarekk@gmail.com>
2022-01-25 10:20:27 +0100

    fixing entity class

--- src/main/java/com/scanner/demo/entities/Laptop.java
+++ src/main/java/com/scanner/demo/entities/Laptop.java
@@ -9,10 +9,10 @@ import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
 @Slf4j
 
 public class Laptop {
@@ -31,6 +31,158 @@ public class Laptop {
 	private String operatingSystem;
 	private String weight;
 	private String price;
+	
+	
+
+	public Laptop() {
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	public Laptop(String title, String source, String uri, String name, String reference, String imageUri,
+			String screenSize, String screenResolution, String cpu, String gpu, String ram, String storage,
+			String operatingSystem, String weight, String price) {
+		super();
+		this.title = title;
+		this.source = source;
+		this.uri = uri;
+		this.name = name;
+		this.reference = reference;
+		this.imageUri = imageUri;
+		this.screenSize = screenSize;
+		this.screenResolution = screenResolution;
+		this.cpu = cpu;
+		this.gpu = gpu;
+		this.ram = ram;
+		this.storage = storage;
+		this.operatingSystem = operatingSystem;
+		this.weight = weight;
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getUri() {
+		return uri;
+	}
+
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getReference() {
+		return reference;
+	}
+
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+
+	public String getImageUri() {
+		return imageUri;
+	}
+
+	public void setImageUri(String imageUri) {
+		this.imageUri = imageUri;
+	}
+
+	public String getScreenSize() {
+		return screenSize;
+	}
+
+	public void setScreenSize(String screenSize) {
+		this.screenSize = screenSize;
+	}
+
+	public String getScreenResolution() {
+		return screenResolution;
+	}
+
+	public void setScreenResolution(String screenResolution) {
+		this.screenResolution = screenResolution;
+	}
+
+	public String getCpu() {
+		return cpu;
+	}
+
+	public void setCpu(String cpu) {
+		this.cpu = cpu;
+	}
+
+	public String getGpu() {
+		return gpu;
+	}
+
+	public void setGpu(String gpu) {
+		this.gpu = gpu;
+	}
+
+	public String getRam() {
+		return ram;
+	}
+
+	public void setRam(String ram) {
+		this.ram = ram;
+	}
+
+	public String getStorage() {
+		return storage;
+	}
+
+	public void setStorage(String storage) {
+		this.storage = storage;
+	}
+
+	public String getOperatingSystem() {
+		return operatingSystem;
+	}
+
+	public void setOperatingSystem(String operatingSystem) {
+		this.operatingSystem = operatingSystem;
+	}
+
+	public String getWeight() {
+		return weight;
+	}
+
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public static org.slf4j.Logger getLog() {
+		return log;
+	}
 
 	public boolean checkNotNull(Laptop laptop) throws IllegalAccessException {
 		if ((laptop.getTitle() != null) || (laptop.getSource() != null) || (laptop.getUri() != null)
